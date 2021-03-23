445015740cac0ba44495add63b495e44c924b551
Matheus Lopes <matlopes1999@gmail.com>
2021-03-23 00:00:41 -0300

    Adiciona dotenv e o step inicial do projeto

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello Word!');
+import * as dotenv from 'dotenv';
+import path from 'path';
+import express from 'express';
+import { json } from 'body-parser';
+
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
+const app = express();
+
+app.use(json());
+
+app.listen(3000, () => {
+    console.log(`server running into http://localhost:${process.env.PORT}`);
+});
