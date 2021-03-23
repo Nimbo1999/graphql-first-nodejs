3bf4bbb3d0ed8a0fbdc1e9bbc741214a7c7b9a76
Matheus Lopes <matlopes1999@gmail.com>
2021-03-23 00:11:49 -0300

    Realiza a conexão com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import path from 'path';
 import express from 'express';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
@@ -9,6 +10,15 @@ const app = express();
 
 app.use(json());
 
-app.listen(3000, () => {
-    console.log(`server running into http://localhost:${process.env.PORT}`);
+const { DB_HOST, DB_COLLECTION, DB_USER, DB_PASS, PORT } = process.env;
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}.njrgo.mongodb.net/${DB_COLLECTION}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => {
+    app.listen(PORT, () => {
+        console.log(`server running into http://localhost:${process.env.PORT}`);
+    });
+})
+.catch(err => {
+    throw err;
 });
